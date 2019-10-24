e4ae3ee93cbd87f67b4d2227476cac3f70c7ef45
Valerie Woolard <valerie.woolard@gmail.com>
2019-10-24 00:12:31 -0400

    Add fields and methods to loan + specs

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
 class Loan < ApplicationRecord
-  belongs_to :cart
+  DEFAULT_CHECKOUT_PERIOD_IN_DAYS =  14
+
   belongs_to :carrier
 
-  validates :cart, :carrier, :due_date, presence: true
+  belongs_to :member, class_name: "User"
+  validates :member, presence: true
+
+  belongs_to :checkout_volunteer, class_name: 'User'
+  validates :checkout_volunteer, presence: true
+
+  belongs_to :checkin_volunteer, class_name: 'User', optional: true
+
+  validates :carrier, :due_date, presence: true
+
+  before_validation :set_due_date, on: :create
+
+  def set_due_date
+    self.due_date = DEFAULT_CHECKOUT_PERIOD_IN_DAYS.days.from_now.utc.to_date if due_date.nil?
+  end
+
+  def checkin(volunteer)
+    self.update(checkin_volunteer: volunteer, returned_at: Time.now.utc)
+  end
 end
---
+++ db/migrate/20191024023740_merge_cart_and_loan.rb
@@ -0,0 +1,11 @@
+class MergeCartAndLoan < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :loans, :checkin_volunteer, foreign_key: { to_table: :users }
+    add_reference :loans, :checkout_volunteer, foreign_key: { to_table: :users }
+    add_reference :loans, :member, foreign_key: { to_table: :users }
+    add_column :loans, :returned_at, :datetime
+    remove_column :loans, :cart_id
+
+    drop_table :carts
+  end
+end
